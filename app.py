28430359297b2edc71072202bd7dc74a7ca8ca90
shoulderlin <shoulder.lin@gmail.com>
2024-04-25 09:08:47 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import requests,os
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+#tokens
+LineNotifyToken = os.environ.get['LineNotifyToken']
+ChannelAccessToken = os.environ.get['ChannelAccessToken']
+ChannelSecret = os.environ.get['ChannelSecret']
+
+def lineNotify(msg):
+    token = LineNotifyToken
+    url = "https://notify-api.line.me/api/notify"
+    headers = {
+        "Authorization": "Bearer " + token
+    }   
+    payload = {'message': msg}
+    # files = {'imageFile': open(picURI, 'rb')}
+    r = requests.post(url, headers = headers, params = payload)
+    return r.status_code
+def getKeyWord():
+    with open('kw.txt','r',encoding='utf8')as f:
+        kw= f.read().split(',')
+    return kw
+
+kw = getKeyWord()
+
+app = Flask(__name__)
+
+# Channel access token:(選取自己的機器人→Messaging API→Channel access token)
+line_bot_api = LineBotApi(ChannelAccessToken)
+# Channel secret:(選取自己的機器人→Basic settings→Channel secret)
+handler = WebhookHandler(ChannelSecret)
+
+@ app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@ handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text == '更新關鍵字':
+        kw = getKeyWord()
+        lineNotify(f'關鍵字已更新，共{len(kw)}個關鍵字')
+    else:
+        Tags =[]
+        for k in kw:
+            if k in event.message.text:
+                Tags.append(f'#{k}')
+        if len(Tags)>0:
+            Tags = ' '.join(Tags)
+            msg= f'''{datetime.now().strftime('%Y%m%d %H:%M')}
+{event.message.text}
+
+{Tags}
+            '''
+            lineNotify(msg)
+    # if event.message.text =='a':
+        # msg = (TextSendMessage(text='這是測試'))
+        # line_bot_api.reply_message(event.reply_token, msg)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5050))
+    app.run(host='0.0.0.0', port=port)
